11c812a7c7d7093a7fc12cf9aff85effb2b0e4f9
faceless49 <digital.zeus@mail.ru>
2022-04-14 08:14:16 +0300

    refactor consts to outside. Remove flex center

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -7,32 +7,28 @@ import Link from 'next/link';
 import { Photo } from '../Photo';
 
 import { PhotosType } from 'types';
+import { GUTTER_COLUMN, GUTTER_ROW } from 'vars';
 
 type GalleryPropsType = {
   photos: PhotosType[];
 };
 
-export const Gallery: FC<GalleryPropsType> = ({ photos }) => {
-  const GUTTER_COLUMN = 5;
-  const GUTTER_ROW = 0;
-
-  return (
-    <Row justify="center" gutter={[GUTTER_COLUMN, GUTTER_ROW]}>
-      {photos.map(({ id, url, thumbnailUrl, title, ...restProps }) => (
-        <Col key={id} span={7} offset={0}>
-          <Link href={`/photo/${id}`} passHref>
-            <a href={`/photo/${id}`}>
-              <Photo
-                thumbnailUrl={thumbnailUrl}
-                url={url}
-                title={title}
-                {...restProps}
-                id={id}
-              />
-            </a>
-          </Link>
-        </Col>
-      ))}
-    </Row>
-  );
-};
+export const Gallery: FC<GalleryPropsType> = ({ photos }) => (
+  <Row gutter={[GUTTER_COLUMN, GUTTER_ROW]}>
+    {photos.map(({ id, url, thumbnailUrl, title, ...restProps }) => (
+      <Col key={id} span={7} offset={1}>
+        <Link href={`/photo/${id}`} passHref>
+          <a href={`/photo/${id}`}>
+            <Photo
+              thumbnailUrl={thumbnailUrl}
+              url={url}
+              title={title}
+              {...restProps}
+              id={id}
+            />
+          </a>
+        </Link>
+      </Col>
+    ))}
+  </Row>
+);
--- vars/index.ts
+++ vars/index.ts
@@ -1,1 +1,4 @@
 export const API_URL = 'https://jsonplaceholder.typicode.com/';
+
+export const GUTTER_COLUMN = 0;
+export const GUTTER_ROW = 0;
